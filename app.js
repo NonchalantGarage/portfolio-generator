b4e53f8b9698cc8aa5926992876a7001e6fc0d73
Curtis <hong.curtis@gmail.com>
2021-12-15 20:39:39 -0800

    learned about const and let and arrow function expressions

--- app.js
+++ app.js
@@ -4,3 +4,23 @@ var sum = 6 + 3;
 
 console.log(message);
 console.log(sum);
+
+var commandLineArgs = process.argv;
+console.log(commandLineArgs);
+
+const profileDataArgs = process.argv.slice(2, process.argv.length);
+console.log(profileDataArgs)
+
+const printProfileData = profileDataArr =>{
+    for (let i=0; i < profileDataArr.length; i+=1) {
+        console.log(profileDataArr[i]);
+
+    }
+    console.log('================');
+
+    profileDataArr.forEach((profileItem)=> {
+        console.log(profileItem)
+    });
+};
+
+printProfileData(profileDataArgs);
